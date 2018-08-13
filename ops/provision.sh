9cd380ba4356088e5b4eb2d7f284369ef22e38f6
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-08-13 14:38:21 +0300

    script to install nginx

--- ops/provision.sh
+++ ops/provision.sh
@@ -5,9 +5,17 @@ echo Checking if NGINX is installed
 install_nginx()
 {
 echo "NGINX is not installed, installing" 
-exit 0
+
+$ cat <<EOF > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/x86_64/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y nginx && echo NGINX successfully installed
 }
 
+which nginx && echo NGINX is installed
 which nginx || install_nginx
-
-
